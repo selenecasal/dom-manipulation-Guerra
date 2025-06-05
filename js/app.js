1f33d5a016a876c13fc9ccee72991d1f3a07b2d5
selene-casal <guerracasalselene@gmail.com>
2025-06-05 20:37:53 -0300

    css

--- js/app.js
+++ js/app.js
@@ -1,31 +1,29 @@
 document.getElementById('boton-agregar-tarjeta').addEventListener('click', function() {
-    cont=0;
-    while(cont < 5) {
-        cont++;
-        const br = document.createElement('br');
-        document.getElementById('contenedor-tarjetas').appendChild(br);
-
-    }
-    cont++;
   const nuevaTarjeta = document.createElement('div');
   nuevaTarjeta.className = 'tarjeta';
-  const titulo= document.createElement('h2');
+
+  const titulo = document.createElement('h2');
   titulo.setAttribute('contenteditable', 'true');
   titulo.textContent = 'Título de la Tarjeta';
   nuevaTarjeta.appendChild(titulo);
+  
   const imagen = document.createElement('img');
   imagen.setAttribute('src', 'https://images.pagina12.com.ar/styles/focal_16_9_960x540/public/2021-12/296121-gato-andino-_0.jpg?h=ada05aa9&itok=D8n8mdev');
   nuevaTarjeta.appendChild(imagen);
+  
   const descripcion = document.createElement('p');
   descripcion.setAttribute('contenteditable', 'true');
   descripcion.textContent = 'Descripción de la Tarjeta';
   nuevaTarjeta.appendChild(descripcion);
+  
   const botonEliminar = document.createElement('button');
   botonEliminar.textContent = 'X';
-  nuevaTarjeta.appendChild(botonEliminar)
+  nuevaTarjeta.appendChild(botonEliminar);
   
-  document.getElementById('contenedor-tarjetas').appendChild(nuevaTarjeta);    
-    botonEliminar.addEventListener('click', function() {
-        nuevaTarjeta.remove();
-    });
+  document.getElementById('contenedor-tarjetas').appendChild(nuevaTarjeta);
+
+  botonEliminar.addEventListener('click', function() {
+    nuevaTarjeta.remove();
+    cont--;
+  });
 });
